523ecb1882b743bfe262b04d4ed5c4b1c5407a39
David Rooney <91282834+droonee@users.noreply.github.com>
2022-11-02 12:46:36 -0600

    Update todo.py

--- services/todo.py
+++ services/todo.py
@@ -2,25 +2,19 @@ from flask import Flask, jsonify, make_response
 import json
 import os
 
-print("here")
 app = Flask(__name__)
 
 database_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-print(database_path)
 
 with open('{}/database/todo.json'.format(database_path), "r") as jsf:
     todo_list = json.load(jsf)
 
 @app.route('/', methods=['GET'])
 def hello():
-    ''' Greet the user '''
-
     return "Todo service is up"
 
 @app.route('/lists', methods=['GET'])
 def show_lists():
-    ''' Displays all the lists '''
-
     tlists = []
     for username in todo_list:
         for lname in todo_list[username]:
@@ -29,8 +23,6 @@ def show_lists():
 
 @app.route('/lists/<username>', methods=['GET'])
 def user_list(username):
-    ''' Returns a user oriented list '''
-
     if username not in todo_list:
         return "No list found"
 
